89ee4188310678d64415a79be40108ede1d86d3d
Charanjeet Singh <charanjeetshaktawat@gmail.com>
2020-10-08 10:37:38 +0530

    Added : Player Mark Functionality

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,16 +1,29 @@
+import java.util.Scanner;
 
 public class TicTacToeGame {
+		char array[] =new char[10];
+		public static void main(String[] args) {
+		TicTacToeGame board = new TicTacToeGame();
+		board.initGame();
+		board.inputMark();
+		}	
 
-	static char array[] =new char[10];
-	
-	public static void main(String[] args) {
-		TicTacToeGame.displayGame();
-	}
+			public void initGame(){
+			for(int i=0;i<10;i++){
+			array[i]=' ';
+			}
+		}
 
-static public void displayGame(){
-	for(int i=0;i<10;i++){
-		array[i]=Character.forDigit(0,10);
-		System.out.println(array[i]);
+		public void inputMark() {
+		Scanner sc = new Scanner(System.in);
+		char computerMark, playerMark;
+		System.out.println("Enter Player 1  mark (Select X or O): " );
+		playerMark = sc.next().charAt(0);
+		if (playerMark == 'X'){
+		computerMark='O';
+		}
+		else{
+		computerMark='X';
+		}
 	}
-}
 }
