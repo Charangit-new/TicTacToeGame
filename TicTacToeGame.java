a49ad9895ef56b68a2b27a45e1871c249c6298a8
Charanjeet Singh <charanjeetshaktawat@gmail.com>
2020-10-08 23:56:12 +0530

    Added : Random Toss + Check Filled Cell Functionality

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,9 +1,11 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class TicTacToeGame {
 		char array[] = new char[10];
 		int count=0;
 		char computerMark, playerMark;
+		Random random = new Random();
 		Scanner sc = new Scanner(System.in);
 		public static void main(String[] args) {
 		TicTacToeGame board = new TicTacToeGame();
@@ -45,32 +47,72 @@ public class TicTacToeGame {
 		
 		// get the user choice
 		public void selectMove(char computerMark , char playerMark) {
-			char choice;
-			for (int i = 1; i <=8; i++) {
-					System.out.println("Enter player 1 choice : ");
-					choice = sc.next().charAt(0);
-					replace(choice,playerMark);
-					displayBoard();
-					System.out.println("Enter computer player choice : ");
-					choice = sc.next().charAt(0);
-					replace(choice,computerMark);
-					displayBoard();
+			System.out.println();
+			int num = random.nextInt(2);
+			if (num==1) {
+				System.out.println("Player 1 wins toss");
+				System.out.println();
+				getInput(num);
 			}
-				System.out.println("Enter player 1 choice : ");
-				int choice1 = sc.nextInt();
-				char choice2 = (char) choice1;
-				replace(choice2,playerMark);
-				displayBoard();
+			else{
+				System.out.println("Computer wins toss" );
+				System.out.println();
+				getInput(num);
+			}
 		}
 		
 		
+		public void getInput(int num) {
+			char choice;
+			String user ;
+			if(num==1) {
+				user = "Player 1";
+				System.out.print(user +  " : Enter  choice : ");
+				choice = sc.next().charAt(0);
+				if(checkIfFilled(choice) == true ) {
+						replace(choice,playerMark);
+						displayBoard();
+				}
+				else {
+					getInput(num);
+				}
+			 }
+			else {
+				 user = "Computer";
+				 choice= Character.forDigit(random.nextInt(9)+1 , 10);
+				 System.out.println(user + ": Enter choice : " + choice);
+				 if(checkIfFilled(choice) == true ) {
+						replace(choice,computerMark);
+						displayBoard();
+				}
+				else {
+					getInput(num);
+				}
+		}				
+}	
+
+		// checking for empty choices
+		public boolean checkIfFilled(char choice) {
+			boolean isEmpty=true;
+			for(int i=0;i<array.length;i++){
+				if(array[i] != choice) {
+					isEmpty = false;
+ 				}
+				else {
+					isEmpty = true; 
+					break;
+				}
+			}	
+			return isEmpty;
+		}
+		
+
 		// replace the selected choice with user mark
 		public void replace(char choice,char userMark){
 			for(int i=0;i<array.length ;i++) {
 				if(array[i] == choice){
 					array[i]=userMark;
 				}
-	    }
-			
+	    }	
 	}				
 }
